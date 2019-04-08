e94dd035dd07bff3a9ae0c0ed5124d4649e06362
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-04-09 04:37:58 +0600

    info service is updated

--- Assignment/src/app/info.service.ts
+++ Assignment/src/app/info.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  _getData() {
+     return this.http.get('https://www.miljolare.no/ws/nml/pnc/?service=get_scheme');
+  }
 }
