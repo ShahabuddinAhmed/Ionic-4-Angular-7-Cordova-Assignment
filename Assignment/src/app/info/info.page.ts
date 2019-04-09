a26a2cf0be6a17a5de1e12456b54409ea9672dab
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-04-09 17:26:25 +0600

    some somment is out

--- Assignment/src/app/info/info.page.ts
+++ Assignment/src/app/info/info.page.ts
@@ -1,3 +1,4 @@
+import { InfoService } from './../info.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoPage implements OnInit {
 
-  constructor() { }
+  infos: any;
+
+  constructor(private _infoService: InfoService) { }
 
   ngOnInit() {
+    this.getData();
   }
 
+  public getData() {
+    this._infoService._getData()
+    .subscribe(data => {
+      this.infos = data;
+      this.infos.phenology.map(result => {
+        console.log(result.name);
+        let name = result.name;
+        console.log(name.en);
+        console.log(name.la);
+        console.log(name.no);
+
+        result.phases.map(innerresult => {
+          console.log(innerresult);
+          let id = innerresult.id;
+          console.log(id);
+          let title = innerresult.title;
+          console.log(title.en);
+          console.log(title.no);
+          console.log(innerresult.ws);
+          let ws = innerresult.ws;
+          console.log(ws.add);
+        });
+      });
+    });
+  }
 }
